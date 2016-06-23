38dd960bd4586d4be109f26a70ec2c567eaeee04
Tiago Simão <tiagosimao@gmail.com>
2016-06-23 19:12:23 +0100

    retrying port instantiation on failure

--- fetchy-soap/src/main/java/org/irenical/fetchy/service/factory/soap/SOAPServiceExecutor.java
+++ fetchy-soap/src/main/java/org/irenical/fetchy/service/factory/soap/SOAPServiceExecutor.java
@@ -6,43 +6,43 @@ import org.irenical.fetchy.service.factory.ServiceDiscoveryExecutor;
 import javax.xml.ws.Service;
 import java.net.URI;
 
-public class SOAPServiceExecutor<PORT, ENDPOINT extends Service> extends ServiceDiscoveryExecutor< PORT, PORT > {
+public class SOAPServiceExecutor<PORT, ENDPOINT extends Service> extends ServiceDiscoveryExecutor<PORT, PORT> {
 
-    private final Class< PORT > portClass;
+  private final Class<PORT> portClass;
 
-    private final Class< ENDPOINT > endpointClass;
+  private final Class<ENDPOINT> endpointClass;
 
-    private String address;
+  private String address;
 
-    private PORT port;
+  private PORT port;
 
-    public SOAPServiceExecutor(Class< PORT > portClass, Class< ENDPOINT > endpointClass, String serviceId) {
-        super( serviceId );
+  public SOAPServiceExecutor(Class<PORT> portClass, Class<ENDPOINT> endpointClass, String serviceId) {
+    super(serviceId);
 
-        this.portClass = portClass;
-        this.endpointClass = endpointClass;
+    this.portClass = portClass;
+    this.endpointClass = endpointClass;
+  }
+
+  @Override
+  protected PORT newInstance(ServiceNode serviceNode) throws Exception {
+    if (address == null || !serviceNode.getAddress().equals(address) || port == null) {
+      address = serviceNode.getAddress();
+
+      ServiceClient<ENDPOINT, PORT> serviceClient = new ServiceClient<>(endpointClass, portClass,
+          URI.create(serviceNode.getAddress()));
+      port = serviceClient.getPort();
     }
+    return port;
+  }
 
-    @Override
-    protected PORT newInstance(ServiceNode serviceNode) throws Exception {
-        if ( address == null || ! serviceNode.getAddress().equals( address ) ) {
-            address = serviceNode.getAddress();
+  @Override
+  protected void onBeforeExecute(PORT port) {
 
-            ServiceClient<ENDPOINT, PORT> serviceClient = new ServiceClient<>( endpointClass, portClass,
-                    URI.create( serviceNode.getAddress() ) );
-            port = serviceClient.getPort();
-        }
-        return port;
-    }
+  }
 
-    @Override
-    protected void onBeforeExecute(PORT port) {
+  @Override
+  protected void onAfterExecute(PORT port) {
 
-    }
-
-    @Override
-    protected void onAfterExecute(PORT port) {
-
-    }
+  }
 
 }
