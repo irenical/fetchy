118ba51464950d12bac7a1cf72c19023076c89b4
Diogo Neves <dsneves@impresa.pt>
2017-09-03 03:02:38 +0100

    optional client configurator

---
+++ fetchy-connector-jersey2/src/test/java/org/irenical/fetchy/connector/jersey2/Jersey2ConnectorTest.java
@@ -0,0 +1,46 @@
+package org.irenical.fetchy.connector.jersey2;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.irenical.fetchy.Node;
+import org.irenical.fetchy.connector.Stub;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import java.util.function.Consumer;
+
+import static org.mockito.Mockito.times;
+
+public class Jersey2ConnectorTest {
+
+    @Test
+    public void testNoClientConfigurator() throws Exception {
+        Jersey2Connector connector = new Jersey2Connector();
+
+        Stub<WebTarget> connect = connector.connect(new Node( "localhost", 1337 ) );
+
+        Assert.assertTrue( connect.get().getConfiguration().isRegistered( JacksonFeature.class ) );
+    }
+
+    @Test
+    public void testWithClientConfigurator() throws Exception {
+        Consumer<ClientBuilder> clientConfigurator = new Consumer<ClientBuilder>() {
+            @Override
+            public void accept(ClientBuilder clientBuilder) {
+
+            }
+        };
+
+        Consumer<ClientBuilder> clientConfiguratorSpy = Mockito.spy( clientConfigurator );
+
+        Jersey2Connector connector = new Jersey2Connector()
+                .withClientConfigurator( clientConfiguratorSpy );
+
+        connector.connect(new Node("localhost", 1337));
+
+        Mockito.verify( clientConfiguratorSpy, times( 1 ) ).accept( Mockito.any() );
+    }
+
+}
